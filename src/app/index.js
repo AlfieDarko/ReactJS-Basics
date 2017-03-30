ad7c9df50534b3f6d7b18304266444cccabd95ca
Alfie Darko <mrhazzy@gmail.com>
2017-03-30 02:28:27 +0100

    nearly completed course, messed around with components and state

--- src/app/index.js
+++ src/app/index.js
@@ -4,24 +4,44 @@ import { render } from 'react-dom';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
 
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      homeLink: 'Home'
+    };
+  }
+
+  onGreet(){
+      alert('Hello!');
+    }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    });
+  }
+
   render(){
-      return (
+          return (
           <div className="container">
               <div className="row">
                   <div className="col-xs-10 col-xs-offset-1">
-
-                    <Header/>
+                    <Header homeLink={this.state.homeLink}/>
 
                   </div>
                 </div>
 
               <div className="row">
                   <div className="col-xs-10 col-xs-offset-1">
+                    <Home
+                        name={'Max'}
+                        initialAge={27}
+                        greet={this.onGreet}
+                        changeLink={this.onChangeLinkName.bind(this)}
+                        initialLinkName={this.state.homeLink}
+                        />
 
-                    <Home/>
-                    
                   </div>
                 </div>
           </div>
